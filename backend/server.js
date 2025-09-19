6220c6420ac3527cc27195e1e3ccd6cf19735314
vaishnavi <vaishnavi@ascendion.com>
2025-09-19 11:32:14 +0530

    Initial project structure with basic frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+//Connect to MongoDB
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/banking-app1';
+mongoose.connect(uri)
+.then(()=>console.log('MongoDB connected..'))
+.catch(err => console.error(err));
+
+app.get('/',(req,res)=>{
+    res.send('Backend is running!' );
+
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port:${port}`);
+})
