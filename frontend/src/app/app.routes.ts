70d8c7d447f871fe30e1113cac1159cc871e03b1
vaishnavi <vaishnavi@ascendion.com>
2025-09-19 16:53:19 +0530

    feat:Add login module1

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
-// import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component';
 // import { ProductListComponent } from './product-list/product-list.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
-//   { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
 //   { path: 'products', component: ProductListComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
